6706f193d019681d2fc8339c4e161fd2759fe9f4
Gameunkulus <bugr24-8@mail.ru>
2023-12-10 17:25:38 +0300

    program redacted

---
+++ tests/AutorisationSucces.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+
+test("test", async ({ page }) => {
+    // Go to https://netology.ru/free/management#/
+    await page.goto("https://netology.ru/free/management#/");
+  
+    // Click a
+    await page.click("a");
+    await expect(page).toHaveURL("https://netology.ru/");
+  
+    // Click text=Учиться бесплатно
+    await page.click("text=Войти");
+    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  
+    await page.click(".styles_socialButton__KUXVC.styles_university2035__eZ_jv");
+
+    await expect(page).toHaveURL("https://netology.ru/profile?provider=university2035");
+
+    // Click text=Как перенести своё дело в онлайн
+  });
---
+++ user.js
@@ -0,0 +1,3 @@
+const email = "bugr24-8@mail.ru"
+const password = "26091994"
+
