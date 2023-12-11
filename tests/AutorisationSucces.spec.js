e3b811a568f2cb5797054b2601024b8760c60d90
Gameunkulus <bugr24-8@mail.ru>
2023-12-11 16:00:56 +0300

    program redacted 5

--- tests/AutorisationSucces.spec.js
+++ tests/AutorisationSucces.spec.js
@@ -1,24 +1,31 @@
-const { test, expect } = require('@playwright/test');
+const {
+  test,
+  expect
+} = require('@playwright/test');
 
 const user = require("../user/user");
 const email = user.email;
 const password = user.password;
 
-test("test", async ({ page }) => {
-    // Go to https://netology.ru/free/management#/
-    await page.goto("https://netology.ru/free/management#/");
-  
-    // Click a
-    await page.click("a");
-    await expect(page).toHaveURL("https://netology.ru/");
-  
-    // Click text=Учиться бесплатно
-    await page.click("text=Войти");
-    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
-  
-    await page.click(".styles_socialButton__KUXVC.styles_university2035__eZ_jv");
+test("test", async ({
+  page
+}) => {
+  // Go to https://netology.ru/free/management#/
+  await page.goto("https://netology.ru/");
 
-    await expect(page).toHaveURL("https://netology.ru/profile?provider=university2035");
+  // Click a
+  await page.click("a");
+  await expect(page).toHaveURL("https://netology.ru/");
 
-    // Click text=Как перенести своё дело в онлайн
-  });
+  // Click text=Учиться бесплатно
+  await page.click("text=Войти");
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+
+  await expect(page).toHaveURL("https://netology.ru/profile?provider=university2035");
+
+  // Click text=Как перенести своё дело в онлайн
+});
